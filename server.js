f09b1908a866617c821771b0f0f1ef915d301f9e
Max Edell <maxed@adobe.com>
2018-10-20 17:35:26 -0400

    fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const dbName = 'MapData',
+"use strict";
+const dbName = 'data.db',
     tableNames = {
         events: 'defaultConferenceEvents',
         sessions: 'sessions',
@@ -14,34 +15,50 @@ const dbName = 'MapData',
     port = process.env.PORT || 9999;
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(`data/${dbName}.db`);
+const db = new sqlite3.Database(`data/${dbName}`);
 const express = require('express');
 const api = express();
 
-api.get(`*/*`, (req, res) => {
+api.get(`/favicon.ico`, (req,res) => {
+    res.status(404);
+});
+
+api.get(`*`, (req, res) => {
     const route = req.url.split('/')[1],
         singular = route.slice(0, -1),
         param = req.url.split('/')[2];
-    db.get(`SELECT * FROM ${tableNames[route]} WHERE ${idNames[route]} = ${param}`, 
-        (err, row) => {
-            if(err) {
-                res.status(503);
-                res.send('No good, sorry m8.');
-            }
-            res.json({ singular : row });
-    });
-});
-
-api.get('*', (req, res) => {
-    const route = req.url.split('/')[1];
-    db.all(`SELECT * FROM ${tableNames[route]}`, 
-        (err, all) => {
-            if(err) {
-                res.status(503);
-                res.send('No good, sorry m8.');
-            }
-            res.json({ route : all });
-    });
+    if(param){
+        console.log('Request: ', `SELECT * FROM ${tableNames[route]} WHERE ${idNames[route]} = ${param}`);
+        db.get(`SELECT * FROM ${tableNames[route]} WHERE ${idNames[route]} = ${param}`, 
+            (err, row) => {
+                if(err) {
+                    console.log('error: ', err);
+                    res.status(503);
+                    res.send('No good, sorry m8.');
+                } 
+                else {
+                    let returnObj = {};
+                    returnObj[singular] = row;
+                    res.json(returnObj);
+                }
+        });
+    }
+    else {
+        console.log('Request: ', `SELECT * FROM ${tableNames[route]}`);
+        db.all(`SELECT * FROM ${tableNames[route]}`, 
+            (err, all) => {
+                if(err) {
+                    console.log('error: ', err);
+                    res.status(503);
+                    res.send('No good, sorry m8.');
+                }
+                else{
+                    let returnObj = {};
+                    returnObj[route] = all;
+                    res.json(returnObj);
+                }
+        });
+    }
 });
 
 
